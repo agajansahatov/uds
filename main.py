0d1207873b80ff078f1082f9f17a9cdc2ec7e33e
Agajan Sahatov <agajan.st@gmail.com>
2024-04-27 23:56:56 +0800

    Add video of Project testing

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from src.model_trainer import ModelTrainer
 if __name__ == "__main__":
     # model_trainer = ModelTrainer('./data-mountain/')
     # model_trainer.train_model('./models/', 'agajan_mountain_model')
-    auto_driver = AutoDriver('models/agajan_mountain_model.h5')
+    auto_driver = AutoDriver('models/agajan_lake_model.h5')
     auto_driver.run(15)
